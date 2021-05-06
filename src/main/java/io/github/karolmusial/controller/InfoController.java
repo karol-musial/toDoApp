832b88043cf3751096e0d2f624cf7cc47b39f7de
karol-musial <musialkarol99@gmail.com>
2021-05-06 15:38:53 +0200

    Setting up yaml and adding another controller

---
+++ src/main/java/io/github/karolmusial/controller/InfoController.java
@@ -0,0 +1,28 @@
+package io.github.karolmusial.controller;
+
+import io.github.karolmusial.TaskConfigurationProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+
+    private DataSourceProperties datasource;
+    private TaskConfigurationProperties myProp;
+
+    public InfoController(DataSourceProperties datasource, TaskConfigurationProperties myProp) {
+        this.datasource = datasource;
+        this.myProp = myProp;
+    }
+
+    @GetMapping("info/url")
+    String url(){
+        return datasource.getUrl();
+    }
+
+    @GetMapping("info/prop")
+    boolean myProp(){
+        return myProp.getTemplate().isAllowMultipleTasks();
+    }
+}
